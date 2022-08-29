81f748672e9592bcc6f1e98c866f2533fd4dd2f7
MKortmann <companymvms@gmail.com>
2022-08-29 22:58:42 +0200

    add: react-bootstrap bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css'
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Stack from 'react-bootstrap/Stack'
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>My Reads</h1>
-		</div>
+		<ThemeProvider
+			breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+			minBreakpoint='xxs'>
+			<Container>
+				<Stack gap={3}>
+					<div className='bg-light border'>First item</div>
+					<div className='bg-light border'>Second item</div>
+					<div className='bg-light border'>Third item</div>
+				</Stack>
+			</Container>
+		</ThemeProvider>
 	)
 }
 
